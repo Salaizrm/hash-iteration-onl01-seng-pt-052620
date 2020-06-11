277006324b73fdfe4d0c32a2b28dadf22d8b122c
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-11 02:31:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do | k, v |
     if v < 12
       puts "Happy Birthday #{k}! You are now #{v} years old!"
+    else
+      puts "You are too old for this."
     end
   end
 end
